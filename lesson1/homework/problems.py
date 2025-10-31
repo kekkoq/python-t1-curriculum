465af9ae81729c095648580eaf408e117e79a28e
kekkoq <kekkoq@gmail.com>
2025-10-30 18:33:04 -0700

    Complete lesson1/homework

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,34 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+lucky_number = 12
+print("My lucky number is:", lucky_number)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+hours_i_sleep = 0
+print("I sleep for", hours_i_sleep, "hours")
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fav_fruit = "pineapple"
+print("My_favourite_fruit_is", fav_fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
+city = "redmond"
+country = "united_states"
+print(city)
+Print(country)
 
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+
+name = "mateo"
+age = 6
+type = "wiener_dog"
+
+print("My_dog's_name_is_" mateo "his_age_is_" age "and his type is" wiener_dog)
